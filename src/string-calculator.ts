cf8fba67a00b87c0da814f6f09966c0fdeccb7b8
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-01 11:36:56 +0100

    Green: Should sum zero if no numbers, case a

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,2 +1,4 @@
 
-export const add_number = (operation: string) => operation != '' ? Number(operation) : 0;
+export const add_number = (operation: string) => {
+  return isNaN(Number(operation)) ? 0 : Number(operation);
+};
