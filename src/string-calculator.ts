ba9c316995c757fb86a78a50bc78ea1e5cde4a65
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-01 14:06:47 +0100

    Green: Should show all negatives in the exception message

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -9,7 +9,14 @@ const sumNumbers = (value1: number, value2: number) => value1 + value2;
 const checkPositive = (numbers: Array<number>) => {
   const filtered_numbers = numbers.filter(number => number < 0);
   if (filtered_numbers.length > 0) {
-    throw Error('Negatives not allowed: ' + filtered_numbers[0]);
+    let filtered_numbers_string = '';
+    for (let i = 0; i < filtered_numbers.length; i++) {
+      filtered_numbers_string += filtered_numbers[i];
+      if (i < filtered_numbers.length - 1) {
+        filtered_numbers_string += ', ';
+      }
+    }
+    throw Error('Negatives not allowed: ' + filtered_numbers_string);
   }
 };
 
