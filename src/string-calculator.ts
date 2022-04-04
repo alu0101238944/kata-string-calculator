a2bd3e9cea0bb1a8e07146ffab398a8cc307327d
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-04 12:09:00 +0100

    Refactor: Should sum numbers separated with commas and newlines

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,22 +1,22 @@
 
-const recursiveStringCalculator = (expression: string, acc: string) => {
+const recursiveStringCalculator = (expression: string, acc: string, delimiters: Array<string>) => {
   const isEmptyExpression = expression.length === 0;
   const currentChar = expression[0];
-  const isDelimiterCurrentChar = [',', '\n'].includes(currentChar);
+  const isDelimiterCurrentChar = delimiters.includes(currentChar);
   let result = 0;
   if (isEmptyExpression || isDelimiterCurrentChar) {
     result += Number(acc);
   }
   if (!isEmptyExpression) {
     if (isDelimiterCurrentChar) {
-      result += recursiveStringCalculator(expression.slice(1), '');
+      result += recursiveStringCalculator(expression.slice(1), '', delimiters);
     } else {
-      result += recursiveStringCalculator(expression.slice(1), acc + currentChar);
+      result += recursiveStringCalculator(expression.slice(1), acc + currentChar, delimiters);
     }
   }
   return result;
 };
 
 export const stringCalculator = (expression: string) => {
-  return recursiveStringCalculator(expression, '');
+  return recursiveStringCalculator(expression, '', [',', '\n']);
 };
