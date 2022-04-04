c3beb7d6cb93b2a8ecd342eee615dfffea820451
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-04 11:35:43 +0100

    Red: Should sum numbers separated with commas

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,4 +1,9 @@
 
 export const stringCalculator = (expression: string) => {
-  return 0;
+  if (expression.length === 0) {
+    return 0;
+  }
+  const current_number = Number(expression[0]);
+  return (isNaN(current_number) ? 0 : current_number) +
+      stringCalculator(expression.slice(1));
 };
--- test/string-calculator-test.spec.ts
+++ test/string-calculator-test.spec.ts
@@ -4,3 +4,9 @@ import {stringCalculator} from '../src/string-calculator';
 test('Should sum zero if empty string', () => {
   expect(stringCalculator('')).toEqual(0);
 });
+
+test('Should sum numbers separated with commas', () => {
+  expect(stringCalculator('1')).toEqual(1);
+  expect(stringCalculator('1,2')).toEqual(3);
+  expect(stringCalculator('1,23')).toEqual(24);
+});
