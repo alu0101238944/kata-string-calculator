24938a41831753d16360903fbd3c321f97a49d91
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-04-08 09:44:06 +0100

    Improving test naming

--- test/string-calculator-test.spec.ts
+++ test/string-calculator-test.spec.ts
@@ -15,16 +15,16 @@ test('Should sum numbers separated with commas and newlines', () => {
   expect(stringCalculator('1,23\n76')).toEqual(100);
 });
 
-test('Should change the default delimiter using "//[delimiter]\\n[numbers…]"', () => {
+test('Should change the default delimiter', () => {
   expect(stringCalculator('//;\n1;2')).toEqual(3);
 });
 
-test('Should throw an exception "Negatives not allowed" - and the negative that was passed', () => {
+test('Negatives not allowed, and show the negative that was passed', () => {
   expect(() => stringCalculator('//;\n-1;2')).toThrowError('Negatives not allowed: -1');
   expect(() => stringCalculator('//;\n1;-2')).toThrowError('Negatives not allowed: -2');
 });
 
-test('Should show all negatives in the exception message', () => {
+test('Show all passed negatives', () => {
   expect(() => stringCalculator('//;\n-1;-2')).toThrowError('Negatives not allowed: -1,-2');
 });
 
@@ -32,10 +32,10 @@ test('Should ignore numbers bigger than 1000', () => {
   expect(stringCalculator('//;\n6;3;1;1001')).toEqual(10);
 });
 
-test('Should be possible to use delimiters of any length with the following format: "//[delimiter]\\n"', () => {
+test('Should be possible to use delimiters of any length', () => {
   expect(stringCalculator('//[***]\n1***2***3')).toEqual(6);
 });
 
-test('Should allow multiple delimiters like this: "//[delim1][delim2]\\n"', () => {
+test('Should allow multiple delimiters', () => {
   expect(stringCalculator('//[*][%]\n1*2%3')).toEqual(6);
 });
