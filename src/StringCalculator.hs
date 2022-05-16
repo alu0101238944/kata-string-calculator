7dd72db51121728d0c5595e70d5d81029d77469b
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-05-16 15:10:49 +0100

    Should not allow negative numbers

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -1,11 +1,18 @@
 module StringCalculator where
 
 import Data.List.Split (splitOn, splitOneOf)
+import Data.Bifunctor (first)
 
-sumNumbersInExpression :: String -> Int
-sumNumbersInExpression "" = 0
-sumNumbersInExpression ('/' : '/' : delimiter : '\n' : rest) = sumNumbersSeparatedByDelimiters [delimiter] rest
-sumNumbersInExpression expression = sumNumbersSeparatedByDelimiters "\n," expression
+sumNumbersInExpression :: String -> Either Int [Int]
+sumNumbersInExpression "" = Left 0
+sumNumbersInExpression ('/' : '/' : delimiter : '\n' : rest) =
+    first sum $ checkNegatives $ parseNumbersSeparatedByDelimiters [delimiter] rest
+sumNumbersInExpression expression = first sum $ checkNegatives $ parseNumbersSeparatedByDelimiters "\n," expression
 
-sumNumbersSeparatedByDelimiters ::  [Char] -> String -> Int
-sumNumbersSeparatedByDelimiters delimiters = sum . map read . splitOneOf delimiters
+checkNegatives :: [Int] -> Either [Int] [Int]
+checkNegatives list
+  | any (< 0) list = Right $ filter (< 0) list
+  | otherwise = Left list
+
+parseNumbersSeparatedByDelimiters :: [Char] -> String -> [Int]
+parseNumbersSeparatedByDelimiters delimiters = map read . splitOneOf delimiters
--- test/StringCalculatorTest.hs
+++ test/StringCalculatorTest.hs
@@ -11,18 +11,21 @@ main = hspec stringCalculatorTests
 stringCalculatorTests :: Spec
 stringCalculatorTests = describe "StringCalculator behaviour" $ do
   it "An empty expression should evaluate to zero" $ do
-    sumNumbersInExpression "" `shouldBe` 0
+    sumNumbersInExpression "" `shouldBe` Left 0
 
   it "An expression with only a number should evaluate to that number" $ do
-    sumNumbersInExpression "1" `shouldBe` 1
-    sumNumbersInExpression "4" `shouldBe` 4
+    sumNumbersInExpression "1" `shouldBe` Left 1
+    sumNumbersInExpression "4" `shouldBe` Left 4
 
   it "An expression of numbers separated with commas should evaluate to the sum of those numbers" $ do
-    sumNumbersInExpression "1,2" `shouldBe` 3
-    sumNumbersInExpression "10,20,30" `shouldBe` 60
+    sumNumbersInExpression "1,2" `shouldBe` Left 3
+    sumNumbersInExpression "10,20,30" `shouldBe` Left 60
 
   it "An expression of numbers separated with commas or newlines should evaluate to the sum of those numbers" $ do
-    sumNumbersInExpression "1,2\n3" `shouldBe` 6
+    sumNumbersInExpression "1,2\n3" `shouldBe` Left 6
 
   it "Should be posible define custom delimiters" $ do
-    sumNumbersInExpression "//;\n1;2" `shouldBe` 3
+    sumNumbersInExpression "//;\n1;2" `shouldBe` Left 3
+
+  it "Should not allow negative numbers" $ do
+    sumNumbersInExpression "//;\n-1;2;-3" `shouldBe` Right [-1, -3]
